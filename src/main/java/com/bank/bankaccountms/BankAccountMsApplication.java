f2c94359e820e2668a4da3d39b86c127c8393c78
JoelTercero <joel_3p@hotmail.com>
2026-07-06 21:29:57 -0600

    chore:Initial commit

---
+++ src/main/java/com/bank/bankaccountms/BankAccountMsApplication.java
@@ -0,0 +1,13 @@
+package com.bank.bankaccountms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAccountMsApplication.class, args);
+	}
+
+}
